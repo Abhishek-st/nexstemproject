faa30e7b799c70b28dcfa77eeb94beb33b2392ae
Abhishek-st <ankit2632000@gmail.com>
2021-07-22 00:35:17 +0530

    added function extracting data from csv file and sending it to socket and then displaying it using chart.js

---
+++ django-hiring-main/README.md
@@ -0,0 +1,5 @@
+# Hiring Task - Django
+
+Fork the repo and get all relevant files required to complete the task
+
+<strong> Note: Sharing this repo link to others is strictly prohibited </strong>
--- testsocket.py
+++ testsocket.py
@@ -3,23 +3,18 @@ import requests
 import redis
 import websocket
 import random,time
+import csv
 
 ws=websocket.WebSocket()
 ws.connect('ws://localhost:8000/ws/polData/')
 
-for i in range(1000):
-    time.sleep(3)
-    ws.send(json.dumps({'value':random.randint(1,100)}))
+# for i in range(1000):
+#     time.sleep(0.3)
+#     randomlist = random.sample(range(1, 100), 5)
+#     ws.send(json.dumps({'value':randomlist}))
 
-
-# import asyncio
-# import websockets
-
-# async def hello():
-#     uri = "ws://localhost:8000/ws/polData/"
-#     async with websockets.connect(uri) as ws:
-#         for i in range(1000):
-#             time.sleep(3)
-#             ws.send(json.dumps({'value':random.randint(1,100)}))
-
-# asyncio.get_event_loop().run_until_complete(hello())
+with open("django-hiring-main/data.csv") as csvfile:
+    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC) # change contents to floats
+    for row in reader: # each row is a list
+        time.sleep(0.3)
+        ws.send(json.dumps({'value':row}))
